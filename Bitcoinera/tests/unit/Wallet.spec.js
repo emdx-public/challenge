964de1051e5253ec1fb0d00a70d736b2073e3097
Bitcoinera <anagjordano@gmail.com>
2021-08-18 14:53:36 +0200

    add unit tests

---
+++ Bitcoinera/tests/unit/Wallet.spec.js
@@ -0,0 +1,25 @@
+import { shallowMount } from "@vue/test-utils"
+import Wallet from '../../src/views/Wallet.vue'
+
+describe(`Wallet`, () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(Wallet, {
+      propsData:  {
+        msg: 'This is demo net work',
+        address: '',
+        web3: null,
+        balance: 0,
+        BalanceInterval: null,
+        amount: 0.1,
+        receiver: '0x63D8fdD998834accC847b55705135d3EE1B7bc90'
+    },
+      stubs: [`router-link`],
+    })
+  })
+
+  it(`should show the Wallet page`, async () => {
+    expect(wrapper.element).toMatchSnapshot()
+  })
+})
