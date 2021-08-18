4c2ab81d1521522e4f416cc837edd8426bbc8446
Bitcoinera <anagjordano@gmail.com>
2021-08-18 17:30:40 +0200

    prepare navbar for mobile

--- Bitcoinera/src/components/Navbar.vue
+++ Bitcoinera/src/components/Navbar.vue
@@ -16,16 +16,28 @@
                 <div
                     class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto"
                 >
-                    <div class="text-sm lg:flex-grow">
+                    <div class="text-md lg:flex-grow">
                         <router-link
                             v-for="l in links"
                             :key="l.title"
                             :to="l.to"
-                            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+                            class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-teal-200 mr-20"
                         >
                             {{ l.title }}
                         </router-link>
                     </div>
+                </div>
+                <div class="header-menu-section">
+                    <template v-if="isMobileApp">
+                        <div v-if="open" class="close-menu" @click="close()">
+                        <i class="material-icons notranslate mobile-menu-action">close</i>
+                        </div>
+                        <div v-if="!open" class="open-menu" @click="show()">
+                        <i class="material-icons notranslate mobile-menu-action"
+                            >more_vert</i
+                        >
+                        </div>
+                    </template>
                 </div>
             </nav>
         </nav>
@@ -37,12 +49,27 @@ import ExchangeIcon from '@/components/ExchangeIcon'
 export default {
     name: 'Navbar',
     components: { ExchangeIcon },
-
     props: {
         links: {
             type: Array,
             default: () => []
         }
+    },
+    data: () => ({
+        open: false,
+    }),
+    computed: {
+        isMobileApp() {
+            return screen.width < 625
+        }
+    },
+    methods: {
+        close() {
+            this.open = false
+        },
+        show() {
+            this.open = true
+        },
     }
 }
 </script>
