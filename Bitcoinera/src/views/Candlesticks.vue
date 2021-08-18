2a87805ce0bbfa7e6041b04491bf92c199d9a227
Bitcoinera <anagjordano@gmail.com>
2021-08-18 17:08:09 +0200

    use bitfinex websocket for candlesticks

--- Bitcoinera/src/views/Candlesticks.vue
+++ Bitcoinera/src/views/Candlesticks.vue
@@ -1,67 +1,42 @@
 <template>
     <v-row>
         <v-col cols="12">
-            <trading-vue :data="this.$data">{{ ohlcv }}</trading-vue>
+            <trading-vue :data="ohlcv" :width="width" :height="height"
+                title-txt="BTCUSD"
+                :toolbar="true"
+                :overlays="overlays">
+            </trading-vue>
         </v-col>
     </v-row>
 </template>
 <script>
 import TradingVue from 'trading-vue-js'
+import { DataCube } from 'trading-vue-js'
+import Data from '@/assets/chartdata.json'
+import TestOverlay from '@/components/TestOverlay.vue'
 
 export default {
     components: { TradingVue },
-
+    props: ['width', 'height'],
     data() {
         return {
-            ohlcv: [
-                [
-                    1563727680000,
-                    10384.09,
-                    10397.76,
-                    10380.49,
-                    10389.86,
-                    14.232304
-                ],
-                [
-                    1563727740000,
-                    10389.85,
-                    10390.0,
-                    10377.01,
-                    10388.16,
-                    35.418958
-                ],
-                [
-                    1563727800000,
-                    10386.03,
-                    10388.16,
-                    10367.79,
-                    10369.58,
-                    39.760436
-                ],
-                [
-                    1563727860000,
-                    10369.58,
-                    10376.33,
-                    10362.6,
-                    10364.54,
-                    117.406486
-                ],
-                [
-                    1563727920000,
-                    10363.82,
-                    10364.54,
-                    10355.36,
-                    10358.01,
-                    10.331995
-                ]
-            ]
+            ohlcv: new DataCube(Data),
+            overlays: [TestOverlay]
         }
-    }
-    // Consumo API con informacion de grafico
-    //     created() {
-    //     this.axios.get("https://api-pub.bitfinex.com/v2/candles/trade:1h:fUSD:a30:p2:p30/hist").then((response) => {
-    //       this.ohlcv = response.data;
-    //     });
-    //   },
+    },
+    // Websocket connected to Bitfinex API
+    created() {
+        const wss = new WebSocket('wss://api-pub.bitfinex.com/ws/2')
+        wss.onopen = () => {
+            wss.send(JSON.stringify({"event":"subscribe", "channel":"ticker", "pair":"BTCUSD"}))
+        }
+        wss.onmessage = (msg) => {
+            let res = JSON.parse(msg.data)
+            if (Array.isArray((res)) && Array.isArray(res[1]) && res[1].length > 2) {
+                const newData = Data.chart.data.push(res[1])
+                this.ohlcv = new DataCube(newData)
+            }
+        }
+    },
 }
 </script>
